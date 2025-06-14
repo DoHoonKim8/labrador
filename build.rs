ad93cfe9db0d181891a2f198f988bd6815063d8d
DoHoonKim8 <dohoon1097819@gmail.com>
2025-06-14 08:26:02 +0000

    Add build.rs

--- build.rs
+++ build.rs
@@ -1,30 +1,75 @@
 // build.rs
-use std::{env, path::PathBuf, process::Command};
+use std::{env, fs, path::PathBuf, process::Command};
 
 fn main() {
     let manifest = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let src = manifest.join("labrador-c");
 
-    // 1) Build the shared library via Makefile
-    //    (this will produce `libdogs.so` in labrador-c)
-    let status = Command::new("make")
-        .arg("libdogs.so")
-        .current_dir(&src)
-        .status()
-        .expect("failed to run make in labrador-c");
-    assert!(status.success(), "make failed");
+    // 1) Gather all .c files under labrador-c
+    let c_files = vec![
+        "greyhound.c",
+        "dachshund.c",
+        "pack.c",
+        "chihuahua.c",
+        "labrador.c",
+        "data.c",
+        "jlproj.c",
+        "polx.c",
+        "poly.c",
+        "polz.c",
+        "ntt.S",
+        "invntt.S",
+        "aesctr.c",
+        "fips202.c",
+        "randombytes.c",
+        "cpucycles.c",
+        "sparsemat.c",
+    ]
+    .into_iter()
+    .map(|f| src.join(f))
+    .collect::<Vec<_>>();
 
-    // 2) Tell Cargo to link against libdogs.so
+    // 2) Invoke gcc to build a shared lib
+    let mut cmd = Command::new("gcc");
+    cmd.args(&[
+        "-std=gnu18",
+        "-Wall",
+        "-Wextra",
+        "-Wmissing-prototypes",
+        "-Wredundant-decls",
+    ]);
+    cmd.args(&[
+        "-Wshadow",
+        "-Wpointer-arith",
+        "-Wno-unused-function",
+        "-fmax-errors=1",
+        "-flto=auto",
+        "-fwrapv",
+    ]);
+    cmd.args(&[
+        "-march=native",
+        "-mtune=native",
+        "-O3",
+        "-fvisibility=hidden",
+    ]);
+    cmd.args(&["-fPIC", "-shared", "-DLOGQ=24", "-I", src.to_str().unwrap()]);
+    for file in &c_files {
+        cmd.arg(file);
+    }
+    cmd.arg("-o").arg(src.join("liblabrador24.so"));
+    let status = cmd.status().expect("failed to build liblabrador24.so");
+    assert!(status.success());
+
+    // 3) Tell Cargo to link
     println!("cargo:rustc-link-search=native={}", src.display());
-    println!("cargo:rustc-link-lib=dylib=dogs");
+    println!("cargo:rustc-link-lib=dylib=labrador24");
 
-    // 3) Generate bindings for the public header
+    // 4) Generate bindings as before...
     let bindings = bindgen::Builder::default()
         .header(src.join("labrador.h").to_string_lossy())
         .clang_arg(format!("-I{}", src.display()))
         .generate()
         .expect("Unable to generate bindings");
-
     bindings
         .write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs"))
         .expect("Couldn't write bindings.rs");
